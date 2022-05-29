b784ab2d63c78dc52b52fd428ea10128d2c0d699
nathan <nathan.menny@etu.utc.fr>
2022-05-29 17:28:48 +0200

    Changing from list to array to store builds in Tiles

--- src/Tuile.cpp
+++ src/Tuile.cpp
@@ -1,7 +1,21 @@
 #include "Tuile.h"
 
+#include "Villes.h"
+#include "Pres.h"
+#include "Abbayes.h"
+#include "Jardins.h"
+#include "Rivieres.h"
+#include "Routes.h"
+
+#include "Surface.h"
+#include "Chemin.h"
+#include "Batiment.h"
+
+
 namespace Carcassonne {
-    Tuile(std::string& id, std::string& zonesSurfaces, std::string& zonesChemins) : ID(id) {
+    Tuile::Tuile(std::string& id, std::string& zonesSurfaces, std::string& zonesChemins) : ID(id) {
+
+
 
     }
 }
--- src/Tuile.h
+++ src/Tuile.h
@@ -2,7 +2,7 @@
 #define __TUILE_H__
 
 #include <string>
-#include <list>
+#include <array>
 
 using namespace std;
 
@@ -10,13 +10,17 @@ namespace Carcassonne {
 
     class Surface;
     class Chemin;
+    class Batiment;
 
     class Tuile {
     private:
+
+        static const size_t NB_ZONES = 8;
+
         const std::string ID; //Identificateur de la carte, pour retrouver son dessin
-        list<Surface*> surfaces; //Tableau de taille 8, representant les zones des bordures, partant de haut-gauche, puis dans le sens horaire
-        //list<Batiment*> batiments;
-        list<Chemin*> chemins; //Tableau de taille, representant les chemin sur chaque côté, partant du haut, puis sens horaire
+        array<Surface*, NB_ZONES> surfaces; //Tableau de taille 8, representant les zones des bordures, partant de haut-gauche, puis dans le sens horaire
+        array<Batiment*, NB_ZONES> batiments;
+        array<Chemin*, NB_ZONES> chemins; //Tableau de taille, representant les chemin sur chaque côté, partant du haut, puis sens horaire
     public:
         Tuile(std::string& id, std::string& zonesSurfaces, std::string& zonesChemins);
 
