1f2e93bdd2e08d2054e47d26ee32cb6eb2f7b9fa
nathan <nathan.menny@etu.utc.fr>
2022-06-06 08:04:33 +0200

    Adding an error when the Tuile format is incorrect

--- src/Pioche.cpp
+++ src/Pioche.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
 #include <fstream>
-#include <vector>
 
 #include "Utils.h"
 #include "CarcassonneException.h"
@@ -23,14 +21,20 @@ namespace Carcassonne {
         string line;
         stream >> line; // skip la premier ligne
         vector<string> champs;
+        size_t nbTuile = 1;
 
         // Parcours chaque ligne du fichier
         while(stream >> line) {
 
             champs = splitString(line, ";");
 
+            if(champs.size() < 2) {
+                throw PiocheException("Format de la " +to_string(nbTuile) +"eme tuile dans le fichier incorrect !");
+            }
+
             // Cree une nouvelle Tuile a partir des champs recuperes
             pioche.push_back(new Tuile(champs[0], champs[1]));
+            nbTuile++;
         }
 	}
 
