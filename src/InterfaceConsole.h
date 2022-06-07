1862f84cdc8e1b44f7ca3a871958b3588d832787
nathan <nathan.menny@etu.utc.fr>
2022-06-07 21:48:28 +0200

    Adding comments to InterfaceConsole

--- src/InterfaceConsole.h
+++ src/InterfaceConsole.h
@@ -38,6 +38,9 @@ namespace Carcassonne {
         /*! \brief Destructeur de la classe */
         ~InterfaceConsole()=default;
 
+        /*! \brief Affiche a l'utilisateur les emplacements ou il peut poser une Tuile
+            \param[in] c La liste des coordonnees ou des Tuiles peuvent etre posees
+        */
         void afficheEmplacementsJouables(const Coordonnees& c) override {
             streamOut << "\nVoici les coups jouables:\n";
             streamOut << "idxCoup => (x ; y)\n";
@@ -49,31 +52,46 @@ namespace Carcassonne {
             }
         }
 
+        /*! \brief Affiche les donnees liees a un joueur
+            \param[in] j Le joueur a afficher
+        */
         void afficheJoueur(const Joueur& j) override {
             streamOut << "\n-- C'est au tour du joueur " << (j.getNumero() + 1) << " de jouer...\n";
         }
 
+        /*! \brief Affiche a l'utilisateur la Tuile courante
+            \param[in] t La tuile a afficher
+        */
         void afficheTuileCourante(const Tuile& t) override {
             streamOut << "Voici la tuile a jouer ce tour la :\n";
 
             t.affiche(streamOut);
         }
 
+        /*! \brief Affiche le plateau a l'utilisateur
+            \param[in] p Le plateau a afficher
+        */
         void affichePlateau(const Plateau& p) override {
             streamOut << "-- Etat du plateau actuel --\n";
 
             p.affiche(streamOut);
         }
 
+        /*! \brief Affiche un message signalant la fin de la partie */
         void afficheFinPartie() override {
             streamOut << "\n\n **** Fin de la partie ****\n\n";
         }
 
+        /*! \brief Affiche le gagnant
+            \param[in] idxJoueur Indice du joueur gagnant
+        */
         void afficheGagnant(const int idxJoueur) override {
             streamOut << "\n*** Felication au joueur " << (idxJoueur+1) << "!!! \n";
         }
 
-
+        /*! \brief Affiche le score
+            \param[in] j Les joueurs participant a la partie
+        */
         void afficheScore(const Joueurs& j) override {
             streamOut << "| Joueurs | Points |\n";
             // Parcours l'ensemble des joueurs
@@ -92,6 +110,9 @@ namespace Carcassonne {
             }
         }
 
+        /*! \brief Demande a l'utilisateur ou il souhaite poser une Tuile
+            \return L'indice de l'action a effectuer
+        */
         size_t demanderOuPoser() override {
            streamOut << "\nOu souhaitez-vous poser la tuile ?\n";
 
@@ -108,6 +129,9 @@ namespace Carcassonne {
         }
 
 
+        /*! \brief Demande a l'utilisateur s'il souhaite attribuer des points aux joueurs
+            \param[in,out] j Les joueurs participant a la partie
+        */
         void demanderPointsAAttribuer(Joueurs& j) override {
             streamOut << "\nY'a-t'il encore des points a attribuer ?(Y/N)\n";
 
